e0f5c4cb148bb6221cfcf3d1380d3204b7651337
vmorales <vmorales@na-at.com.mx>
2022-04-28 16:28:41 -0500

    feat: instructions view, fonts and images customizables

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-window.onload = function() {
+window.onload = function () {
   initIframe();
 };
 
+// events available
+const EVENT_MODULE = {
+  INIT_MODULE: "INIT_MODULE",
+  PROCESS_INIT: "PROCESS_INIT",
+  PROCESS_ERROR: "PROCESS_ERROR",
+  PROCESS_COMPLETED: "PROCESS_COMPLETED",
+  MODULE_READY: "MODULE_READY",
+};
 
 // facetec credentials, provided by Na-at Technologies
 const CREDENTIALS = {
@@ -20,86 +28,114 @@ const CREDENTIALS = {
 }
 
 // optional, the app has default legends and colors
-const CUSTOMIZATION = {
-  fadCustomization: {
-    colors: {
-      primary: "#A70635",
-      secondary: "#A70635",
-      tertiary: "#363636",
-    },
-    buttons: {
-      primary: {
-        backgroundColor: "#A70635",
-        backgroundColorDisabled: "#dcdcdc",
-        labelColor: "#ffffff",
-        labelColorDisabled: "#8e8e8e",
-        border: "1px solid #A70635",
+const CONFIGURATION = {
+  views: {
+    instructions: true
+  },
+  customization: {
+    fadCustomization: {
+      colors: {
+        primary: "#A70635",
+        secondary: "#A70635",
+        tertiary: "#363636",
       },
+      buttons: {
+        primary: {
+          backgroundColor: "#A70635",
+          backgroundColorDisabled: "#dcdcdc",
+          labelColor: "#ffffff",
+          labelColorDisabled: "#8e8e8e",
+          border: "1px solid #A70635",
+        },
+      },
+      fonts: {
+        title: {
+          fontSize: '25px',
+          fontFamily: 'system-ui'
+        },
+        subtitle: {
+          fontSize: '17px',
+          fontFamily: 'system-ui'
+        },
+        content: {
+          fontSize: '15px',
+          fontFamily: 'system-ui'
+        },
+        informative: {
+          fontSize: '12px',
+          fontFamily: 'system-ui'
+        },
+        button: {
+          fontSize: '17px',
+          fontFamily: 'system-ui'
+        }
+      }
+    },
+
+    moduleCustomization: {
+      legends: {
+        initializing: "Iniciando",
+        processing: "procesando",
+        facetec: {
+          accessibilityCancelButton: "Cancelar",
+          feedbackCenterFace: "Centra tu rostro",
+          feedbackFaceNotFound: "Enfoca tu rostro",
+          feedbackMoveAwayWeb: "Aléjate",
+          feedbackMoveWebCloser: "Acércate",
+          feedbackMovePhoneAway: "Aléjate",
+          feedbackMovePhoneCloser: "Acércate",
+          feedbackHoldSteady: "No te muevas",
+          feedbackMoveWebEvenCloser: "Aún más cerca",
+          instructionsHeaderReadyDesktop: "Biometría facial",
+          instructionsMessageReadyDesktop: "Enfoca tu rostro en la guía y da clic en el botón para continuar",
+          instructionsHeaderReadyMobile1: "Biometría facial",
+          instructionsHeaderReadyMobile2: "",
+          instructionsMessageReadyMobile1: "Enfoca tu rostro en la guía y",
+          instructionsMessageReadyMobile2: "da clic en el botón para continuar",
+          actionImReady: "Continuar",
+          resultFacescanUploadMessage: "procesando",
+          retryHeader: "Inténtalo nuevamente",
+          retrySubheaderMessage: "Necesitamos una imagen clara",
+          retryYourImageLabel: "Tu foto",
+          retryIdealImageLabel: "Pose ideal",
+          retryInstructionMessage1: "Sin brillo o iluminación extrema",
+          retryInstructionMessage2: "Expresión neutral, sin sonreír",
+          retryInstructionMessage3: "Demasiado borroso, limpia tu cámara",
+          presessionFrameYourFace: "Enfoca tu rostro en la guía",
+          presessionLookStraightAhead: "Mira al frente",
+          presessionHoldSteady3: "No te muevas por: 3",
+          presessionHoldSteady2: "No te muevas por: 2",
+          presessionHoldSteady1: "No te muevas por: 1",
+          presessionEyesStraightAhead: "Mira al frente",
+          presessionRemoveDarkGlasses: "Quítate los lentes de sol",
+          presessionNeutralExpression: "Expresión neutral, sin sonreír",
+          presessionConditionsTooBright: "Entorno com demasiada luz",
+          presessionBrightenYourEnvironment: "Entorno con poca luz",
+          actionTryAgain: "Aceptar",
+          cameraPermissionHeader: "Permiso de cámara o micrófono desactivado",
+          cameraPermissionMessage: "Por favor revisa la configuración de tu sistema operativo y los ajustes del navegador.",
+          cameraPermissionLaunchSettings: "Aceptar",
+          initializingCamera: "iniciando",
+          initializingCameraStillLoading: "iniciando...",
+          resultSuccessMessage: "Validación correcta",
+          enterFullscreenHeader: "Prueba de vida",
+          enterFullscreenMessage: "Antes de comenzar da clic en el botón de abajo para abrir en pantalla completa",
+          enterFullscreenAction: "Continuar",
+        },
+      },
+      legendsInstructions: {
+        title: 'Prueba de vida',
+        subtitle: 'Enfoca tu rostro en la guía',
+        buttonNext: 'Continuar',
+      }
     },
   },
-
-  moduleCustomization: {
-    legends: {
-      initializing: "Iniciando",
-      processing: "procesando",
-      facetec: {
-        accessibilityCancelButton: "Cancelar",
-        feedbackCenterFace: "Centra tu rostro",
-        feedbackFaceNotFound: "Enfoca tu rostro",
-        feedbackMoveAwayWeb: "Aléjate",
-        feedbackMoveWebCloser: "Acércate",
-        feedbackMovePhoneAway: "Aléjate",
-        feedbackMovePhoneCloser: "Acércate",
-        feedbackHoldSteady: "No te muevas",
-        feedbackMoveWebEvenCloser: "Aún más cerca",
-        instructionsHeaderReadyDesktop: "Biometría facial",
-        instructionsMessageReadyDesktop: "Enfoca tu rostro en la guía y da clic en el botón para continuar",
-        instructionsHeaderReadyMobile1: "Biometría facial",
-        instructionsHeaderReadyMobile2: "",
-        instructionsMessageReadyMobile1: "Enfoca tu rostro en la guía y",
-        instructionsMessageReadyMobile2: "da clic en el botón para continuar",
-        actionImReady: "Continuar",
-        resultFacescanUploadMessage: "procesando",
-        retryHeader: "Inténtalo nuevamente",
-        retrySubheaderMessage: "Necesitamos una imagen clara",
-        retryYourImageLabel: "Tu foto",
-        retryIdealImageLabel: "Pose ideal",
-        retryInstructionMessage1: "Sin brillo o iluminación extrema",
-        retryInstructionMessage2: "Expresión neutral, sin sonreír",
-        retryInstructionMessage3: "Demasiado borroso, limpia tu cámara",
-        presessionFrameYourFace: "Enfoca tu rostro en la guía",
-        presessionLookStraightAhead: "Mira al frente",
-        presessionHoldSteady3: "No te muevas por: 3",
-        presessionHoldSteady2: "No te muevas por: 2",
-        presessionHoldSteady1: "No te muevas por: 1",
-        presessionEyesStraightAhead: "Mira al frente",
-        presessionRemoveDarkGlasses: "Quítate los lentes de sol",
-        presessionNeutralExpression: "Expresión neutral, sin sonreír",
-        presessionConditionsTooBright: "Entorno com demasiada luz",
-        presessionBrightenYourEnvironment: "Entorno con poca luz",
-        actionTryAgain: "Aceptar",
-        cameraPermissionHeader: "Permiso de cámara o micrófono desactivado",
-        cameraPermissionMessage: "Por favor revisa la configuración de tu sistema operativo y los ajustes del navegador.",
-        cameraPermissionLaunchSettings: "Aceptar",
-        initializingCamera: "iniciando",
-        initializingCameraStillLoading: "iniciando...",
-        resultSuccessMessage: "Validación correcta",
-        enterFullscreenHeader: "Prueba de vida",
-        enterFullscreenMessage: "Antes de comenzar da clic en el botón de abajo para abrir en pantalla completa",
-        enterFullscreenAction: "Continuar",
-      },
-    },
-  },
+  pathDependencies: {
+    imageDirectory: 'ASSETS_URL'
+  }
 };
 
-// events available
-const EVENT_MODULE = {
-  INIT_MODULE: "INIT_MODULE",
-  PROCESS_INIT: "PROCESS_INIT",
-  PROCESS_ERROR: "PROCESS_ERROR",
-  PROCESS_COMPLETED: "PROCESS_COMPLETED",
-  MODULE_READY: "MODULE_READY",
-};
+
 
 // errors
 const ERROR_CODE = {
@@ -218,7 +254,7 @@ function initModule() {
   iframe.contentWindow.postMessage(
     new ResponseEvent(EVENT_MODULE.INIT_MODULE, {
       credentials: CREDENTIALS,
-      customization: CUSTOMIZATION,
+      configuration: CONFIGURATION,
     }),
     iframe.src
   );
