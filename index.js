5f38246db6cbb7a86f1c5dc9f47a3d1739c8e3e4
vmorales <vmorales@na-at.com.mx>
2023-07-25 15:18:37 -0600

    refactor: fad-sdk js

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
 window.onload = function () {
- initIframe();
-};
-
-// events available
-const EVENT_MODULE = {
- INIT_MODULE: 'INIT_MODULE',
- PROCESS_INIT: 'PROCESS_INIT',
- PROCESS_ERROR: 'PROCESS_ERROR',
- PROCESS_COMPLETED: 'PROCESS_COMPLETED',
- MODULE_READY: 'MODULE_READY',
+ initProcess();
 };
 
 // facetec credentials, provided by Na-at Technologies
@@ -149,134 +140,52 @@ const CONFIGURATION = {
  },
  pathDependencies: {
   imagesInstructions: {
-   instruction: 'Custom image URL',
+  //  instruction: 'Custom image URL',
   },
   images: {
-   retryScreenSlideshowImage: 'Custom image URL',
+  //  retryScreenSlideshowImage: 'Custom image URL',
   },
  },
 };
 
-// errors
-const ERROR_CODE = {
- NEVER_INITIALIZED: 0,
- MISSING_GUIDANCE_IMAGES: 1,
- TIMEOUT: 2,
- CONTEXT_SWITCH: 3,
- PROGRAMMATICALLY_CANCELLED: 4,
- ORIENTATION_CHANGE_DURING_SESSION: 5,
- LANDSCAPE_MODE_NOT_ALLOWED: 6,
- USER_CANCELLED: 7,
- USER_CANCELLED_FROM_NEW_USER_GUIDANCE: 8,
- USER_CANCELLED_FROM_RETRY_GUIDANCE: 9,
- USER_CANCELLED_WHEN_ATTEMPTING_TO_GET_CAMERA_PERMISSIONS: 10,
- LOCKED_OUT: 11,
- CAMERA_NOT_ENABLED: 12,
- NON_PRODUCTION_MODE_DEVICE_KEY_IDENTIFIER_INVALID: 13,
- DOCUMENT_NOT_READY: 14,
- SESSION_IN_PROGRESS: 15,
- CAMERA_NOT_RUNNING: 16,
- INITIALIZATION_NOT_COMPLETED: 17,
- UNKNOWN_INTERNAL_ERROR: 18,
- USER_CANCELLED_VIA_CLICKABLE_READY_SCREEN_SUBTEXT: 19,
- NOT_ALLOWED_USE_IFRAME_CONSTRUCTOR: 20,
- NOT_ALLOWED_USE_NON_IFRAME_CONSTRUCTOR: 21,
- I_FRAME_NOT_ALLOWED_WITHOUT_PERMISSION: 22,
- STILL_LOADING_RESOURCES: 23,
- RESOURCES_COULD_NOT_BE_LOADED_ON_LAST_INIT: 24,
- RESOURCES_COULD_NOT_BE_LOADED: 25,
- REQUIRED_CREDENTIALS: 26,
-};
+async function initProcess() {
+ const tkn = 'TOKEN';
+ const options = {
+  environment: FadSdk.getFadEnvironments().UAT,
+ };
 
-// models
-class ResponseEvent {
- event;
- data;
- constructor(event, data) {
-  this.event = event;
-  this.data = data;
- }
-}
+ try {
+  const FAD_SDK = new FadSdk(tkn, options);
 
-class Result {
- img; // face image ( base64 )
- imgLowQuality; // low quality face image ( base64 )
- faceScan; // face scan data
- constructor(data) {
-  this.img = data.auditTrail[0];
-  this.imgLowQuality = data.lowQualityAuditTrail[0];
-  this.faceScan = data.faceScan;
- }
-}
+	const facetecResponse = await FAD_SDK.startFacetec(CREDENTIALS, CONFIGURATION);
+	FAD_SDK.end();
+	console.log(facetecResponse);
+	const img = facetecResponse.auditTrail[0];
+	const imgLowQuality = facetecResponse.lowQualityAuditTrail[0];
+	const faceScan = facetecResponse.faceScan;
 
-// subscribe to message event to recive the events from the iframe
-window.addEventListener('message', (message) => {
- // IMPORTANT: check the origin of the data
- if (message.origin.includes('firmaautografa.com')) {
-  if (message.data.event === EVENT_MODULE.MODULE_READY) {
-   // MODULE_READY
-   // call initModule with the specific config
-   initModule();
+	// use the results as you see fit
+	// show result example
+	const containerResult = document.getElementById('container-result');
+	const imageId = document.getElementById('image-id');
+	const imageFace = document.getElementById('image-face');
+	const faceScanElement = document.getElementById('faceScan');
+
+	containerResult.style.display = 'flex';
+	imageId.src = 'data:image/png;base64, ' + img;
+	imageFace.src = 'data:image/png;base64, ' + imgLowQuality;
+	faceScanElement.innerHTML = faceScan;
+ } catch (ex) {
+  // PRROCESS_ERROR
+  console.log(ex);
+  if (ex.code === FadSdk.Errors.Facetec.Session.CAMERA_NOT_RUNNING) {
+   // do something
+   alert('Cámara no soportada, intenta en otro dispositivo');
+  } else if (ex.code === FadSdk.Errors.Facetec.Session.INITIALIZATION_NOT_COMPLETED) {
+   // restart component
+  } else {
+   // restart component
+   alert(JSON.stringify(ex));
   }
-  if (message.data.event === EVENT_MODULE.PROCESS_INIT) {
-   // PROCESS_INIT
-   // only informative
-   console.log('Process init');
-  } else if (message.data.event === EVENT_MODULE.PROCESS_ERROR) {
-   // PRROCESS_ERROR
-   console.log(message.data.data);
-   if (message.data.data.code === ERROR_CODE.CAMERA_NOT_RUNNING) {
-    // do something
-    alert('Cámara no soportada, intenta en otro dispositivo');
-   } else if (message.data.data.code === ERROR_CODE.INITIALIZATION_NOT_COMPLETED) {
-    // restart component
-   } else {
-    // restart component
-    alert(JSON.stringify(message.data.data));
-   }
-  } else if (message.data.event === EVENT_MODULE.PROCESS_COMPLETED) {
-   // PROCESS_COMPLETED
-
-   console.log('Process completed');
-   const result = new Result(message.data.data);
-   const img = result.img;
-   const imgLowQuality = result.imgLowQuality;
-   const faceScan = result.faceScan;
-
-   // use the results as you see fit
-   // show result example
-   const containerResult = document.getElementById('container-result');
-   const containerIframe = document.getElementById('container-iframe-facetec');
-   const imageId = document.getElementById('image-id');
-   const imageFace = document.getElementById('image-face');
-   const faceScanElement = document.getElementById('faceScan');
-
-   containerIframe.style.display = 'none';
-   containerResult.style.display = 'flex';
-   imageId.src = 'data:image/png;base64, ' + img;
-   imageFace.src = 'data:image/png;base64, ' + imgLowQuality;
-   faceScanElement.innerHTML = faceScan;
-  }
- } else return;
-});
-
-function initIframe() {
- // get iframe
- const iframe = document.getElementById('fad-iframe-facetec');
- // url - https://devapiframe.firmaautografa.com/fad-iframe-facetec
- const tkn = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
- const url = `https://devapiframe.firmaautografa.com/fad-iframe-facetec?tkn=${tkn}`;
- // set src to iframe
- iframe.src = url;
-}
-
-function initModule() {
- const iframe = document.getElementById('fad-iframe-facetec');
- iframe.contentWindow.postMessage(
-  new ResponseEvent(EVENT_MODULE.INIT_MODULE, {
-   credentials: CREDENTIALS,
-   configuration: CONFIGURATION,
-  }),
-  iframe.src
- );
+ }
 }
